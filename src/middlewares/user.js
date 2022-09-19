b0afcf72f4e73a08e39aad0a510bfd19ad6b76c5
Jonathan Kechian <jkechian@gmail.com>
2022-09-19 18:09:18 -0300

    arreglos

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -2,38 +2,24 @@ const {todos} = require('../models/usersModel');
 
 
 let middleware = (req,res,next) => {
-<<<<<<< HEAD
 
     let user = null 
 
-=======
-    let user = null 
->>>>>>> cb2d24ab89fd05df5bb5ae27e3521ceba9f6f758
     //caso 1 existe una cookie de usuario
     if(req.cookies && req.cookies.user){
         let users = todos()
         let result = users.find(user => user.email == req.cookies.user)
         req.session.user = result
     }
-<<<<<<< HEAD
-
-
-    //caso 2 existe un usuario en sesion 
-    if(req.session && req.session.user){
-        user = req.session.user
-    }
 
 
 
-    res.locals.user = user
-
-=======
         //caso 2 existe un usuario en sesion 
     if(req.session && req.session.user){
         user = req.session.user
     }
     res.locals.user = user
->>>>>>> cb2d24ab89fd05df5bb5ae27e3521ceba9f6f758
+
     return next()
 }
 
