6eed30f12ba9e8d299bba9debc8ea118c4144d06
MateoGaruti <mateo.gap@gmail.com>
2022-09-30 17:34:39 -0300

    complete el data.sql

--- data.sql
+++ data.sql
@@ -1,9 +1,14 @@
-insert into users (id, firstName,lastName,location,email,password,category, image)
-values (1,"mateo", "ga", "calle", "dh@kechian.com","$2a$10$twQ1dr4ocIusBY8no.KrmuNAVRUzW3rp/tqQV7CbAanNffcBLG.6C","cliente","default.png");
+use kechian
 
+insert into produts (id, name, category, price, discount )
+values (1, "las flores", "small", 10000, 30 )
 
-insert into users (id, firstName,lastName,location,email,password,category, image)
-values (2,"franco", "ma", "calles", "francoMa@kechian.com","$2a$10$twQ1dr4ocIusBY8no.KrmuNAVRUzW3rp/tqQV7CbAanNffcBLG.6FG","admin","default.png");
+insert into produts (id, name, category, price, discount )
+values (2, "CamiSuli", "medium", 12312, 20)
+
+insert into produts (id, name, category, price, discount )
+values (3, "CamiSaki", "large", 17569, 15)
+
 
 insert into sizes (id, size)
 values (1, "small");
@@ -14,3 +19,34 @@ values (2, "medium");
 insert into sizes (id, size)
 values (3, "large");
 
+
+insert into product_size (id, product_id, size.id)
+values (1,5,1);
+
+insert into product_size (id, product_id, size.id)
+values (5,4,2);
+
+insert into product_size (id, product_id, size.id)
+values (7,2,3);
+
+
+
+insert into users (id, firstName,lastName,location,email,password,category, image)
+values (1,"mateo", "ga", "mendoza", "dh@kechian.com","$2a$10$twQ1dr4ocIusBY8no.KrmuNAVRUzW3rp/tqQV7CbAanNffcBLG.6C","cliente","default.png");
+
+
+insert into users (id, firstName,lastName,location,email,password,category, image)
+values (2,"franco", "ma", "jujuy", "francoMa@kechian.com","$2a$10$twQ1dr4ocIusBY8no.KrmuNAVRUzW3rp/tqQV7CbAanNffcBLG.6FG","cliente","default.png");
+
+insert into users (id, firstName,lastName,location,email,password,category, image)
+values (3,"augusto", "flo", "caba", "augustoflo@kechian.com","$2a$10$twQ1dr4ocIusBY8no.KrmuNAASDVRUzW3rp/tqQV7CbAanNffcBLG.6FG","admin","default.png");
+
+
+insert into images (id, product_id)
+values (1,6)
+
+insert into images (id, product_id)
+values (2,7)
+
+insert into images (id, product_id)
+values (3,8)
