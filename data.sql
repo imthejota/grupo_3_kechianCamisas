efefc41b696ea4ea3e459ec1692d44f5b7c9db49
MateoGaruti <mateo.gap@gmail.com>
2022-09-30 14:47:45 -0300

    creacion de data.sql con su contenido

---
+++ data.sql
@@ -0,0 +1,16 @@
+insert into users (id, firstName,lastName,location,email,password,category, image)
+values (1,"mateo", "ga", "calle", "dh@kechian.com","$2a$10$twQ1dr4ocIusBY8no.KrmuNAVRUzW3rp/tqQV7CbAanNffcBLG.6C","cliente","default.png");
+
+
+insert into users (id, firstName,lastName,location,email,password,category, image)
+values (2,"franco", "ma", "calles", "francoMa@kechian.com","$2a$10$twQ1dr4ocIusBY8no.KrmuNAVRUzW3rp/tqQV7CbAanNffcBLG.6FG","admin","default.png");
+
+insert into sizes (id, size)
+values (1, "small");
+
+insert into sizes (id, size)
+values (2, "midium");
+
+insert into sizes (id, size)
+values (3, "large");
+
