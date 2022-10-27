fa759253366b1d7aa219277323712e69c5821fbc
Jonathan Kechian <jkechian@gmail.com>
2022-10-27 17:42:03 -0300

    productValidations ok!

--- src/validations/productValidations.js
+++ src/validations/productValidations.js
@@ -10,20 +10,31 @@
 de talles, colores, etc. que lleguen sean válidos en la base. */
 
 const {body} = require("express-validator");
-const db = require('../database/models/')
+/* const db = require('../database/models/') */
 const path = require('path')
 
 
 let nombre = body('name').notEmpty().withMessage('Campo obligatorio').bail().isLength({min: 5}).withMessage('Mínimo 5 caracteres')
 let descripcion = body('description').notEmpty().withMessage('Campo obligatorio').bail().isLength({min: 20}).withMessage('Mínimo 20 caracteres')
-/* let imagen = body('image').custom((value, {req}) => {
-    let ext = path.extname('image');
-    if (ext != 'jpg' || ext !=  'jpeg'){
-        return Promise.reject ('Formato inválido')
-    }}) */
+let imagen = body('image').custom((value, {req}) => {
+    /* let ext = path.extname(req.files[0].filename);
+    if (ext == 'jpg' || ext ==  'jpeg' || ext == 'gif' || ext == 'png'){
+        return true
+    } else {
+        return Promise.reject('Formato inválido')
+    } */
+    console.log(req.files)
+    let archivo = req.files[0]
+    console.log(archivo)
+    if(archivo.mimetype === 'image/jpg' || archivo.mimetype === 'image/png' || archivo.mimetype === 'image/jpeg' || archivo.mimetype === 'image/gif'){
+        return true
+    } else {
+        return Promise.reject('Formato inválido')
+    }    
+}) 
 
 /* son opcionales let talle =
 let categoria = */ 
 
-let validaciones = [nombre, descripcion/* , imagen */]
+let validaciones = [nombre, descripcion, imagen]
 module.exports = validaciones
