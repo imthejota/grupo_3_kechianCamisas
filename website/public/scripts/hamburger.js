df555edf93ff781769977080cd51a269ae171fec
Jonathan Kechian <jkechian@gmail.com>
2022-11-08 17:01:11 -0300

    menu hamburguesa casi finalizado

--- website/public/scripts/hamburger.js
+++ website/public/scripts/hamburger.js
@@ -1,6 +1,7 @@
 const menuToggle = document.querySelector('.toggle')
-const menuMobile = document.querySelector('menuMobile')
+const menuMobile = document.querySelector('.menuMobile')
 
 menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active')
+    menuToggle.classList.toggle('active');
+    menuMobile.classList.toggle('active');
 })
