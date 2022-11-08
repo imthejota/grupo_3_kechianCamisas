33069139f1afb42659485cfe27402a3695e8aa7b
Jonathan Kechian <jkechian@gmail.com>
2022-11-08 00:41:47 -0300

    terminada animacion, resta listado hambur

---
+++ website/public/scripts/hamburger.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.toggle')
+const menuMobile = document.querySelector('menuMobile')
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active')
+})
