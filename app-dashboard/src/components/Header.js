452b98d1ba1aa7e5b77ac980b1701ee39fa09967
Jonathan Kechian <jkechian@gmail.com>
2022-11-11 21:26:08 -0300

    corrección en react de <a> por Links

--- app-dashboard/src/components/Header.js
+++ app-dashboard/src/components/Header.js
@@ -1,6 +1,7 @@
 import '../styles/header_footer.css'
 import logo from '../assets/logoKechian.png'
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 
 export default class Header extends Component {
@@ -10,14 +11,14 @@ export default class Header extends Component {
                 <header>
                     
                     <nav class="navCentro">
-                        <a href="/"><img class="logo" src={logo} alt=""/></a>
+                        <Link to="/"><img class="logo" src={logo} alt=""/> </Link>
                     </nav> 
 
 
 
 
                     <nav class="navAtras">
-                        <a href="http://localhost:2020" class="buscar"> Volver al sitio </a> 
+                        <Link to="http://localhost:2020" class="buscar"> Volver al sitio </Link>
                     </nav>
                     
 
