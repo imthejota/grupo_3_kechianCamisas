7c4095234eb42c78d33e2aa36354f595e960e463
trinidad echevarria <trinidadechevarria_@hotmail.com>
2022-11-15 00:56:58 -0300

    Cambios en structure.sql y data.sql, funciona perfect

--- website/data.sql
+++ website/data.sql
@@ -1,19 +1,19 @@
 USE kechian;
 
-INSERT INTO categories (name)
-VALUES ("Lisa"),("Estampada");
+INSERT INTO categories (id, name)
+VALUES (DEFAULT, "Lisa"),(DEFAULT, "Estampada");
 
-INSERT INTO products (name, price, discount, description, category_id)
-VALUES ("Camisa Rovira", 8500, 30,"Camisa de lino con cuello sin botones. Moldería Classic fit. Composición: 50% lino, 50% rayón",1),
-("Camisa Vedell", 7000, 20,"Camisa de poplin a cuadros con cuello italiano. Moldería Slim fit. Composición: 100% algodón", 2),
-("Camisa Wescott", 6500, 15,"Camisa de lino con spandex a rayas y cuello solapa. Moldería Slim fit. Composición: 50% lino, 50% rayón", 2),
-("Camisa Anglet", 9000, 10, "Camisa de poplin con cuello italiano. Moldería Classic fit. Composición: 100% algodón", 1);
+INSERT INTO products (name, price, discount, description, image, category_id)
+VALUES ("Camisa Rovira", 8500, 30, "Camisa de lino con cuello sin botones. Moldería Classic fit. Composición: 50% lino, 50% rayón", "default.png", 1),
+("Camisa Vedell", 7000, 20, "Camisa de poplin a cuadros con cuello italiano. Moldería Slim fit. Composición: 100% algodón", "default.png", 2),
+("Camisa Wescott", 6500, 15, "Camisa de lino con spandex a rayas y cuello solapa. Moldería Slim fit. Composición: 50% lino, 50% rayón", "default.png", 2),
+("Camisa Anglet", 9000, 10, "Camisa de poplin con cuello italiano. Moldería Classic fit. Composición: 100% algodón", "default.png", 1);
 
-INSERT INTO sizes (size)
-VALUES ("Small"),("Medium"),("Large"),("Extra large");
+INSERT INTO sizes (id, size)
+VALUES (DEFAULT, "Small"),(DEFAULT, "Medium"),(DEFAULT, "Large"),(DEFAULT, "Extra large");
 
-INSERT INTO product_size (product_id, size_id)
-VALUES (1,2),(2,4),(3,1),(4,3); 
+INSERT INTO product_size (id, product_id, size_id)
+VALUES (DEFAULT, 1, 2),(DEFAULT, 2, 4),(DEFAULT, 3, 1),(DEFAULT, 4, 3); 
 
 INSERT INTO users (firstName, lastName, location, email, password, admin, image)
 VALUES ("Jonathan", "Kechian", "Mendoza", "j@kechian.com","$2a$10$S71t6BVaKWDDPEmietxwme0dN81mzhz5M0mL0LUUA6LqohqfV0Cmq",1 ,"default.png"),
